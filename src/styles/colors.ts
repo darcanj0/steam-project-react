5df7129b347e4d4548ba8468588dfc630da0a3d1
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-30 17:13:45 -0300

    theme config

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  primaryColor: "#0e0f26",
+  secondaryColor: "#302399",
+  tertiaryColor: "#5f23a5",
+  whiteColor: "#fffcff",
+
+  secondaryColorOpacity: "rgba(48, 35, 153, 0.1)",
+
+  glassBorderColor: "rgba(255, 255, 255, 0.18)",
+
+  whiteTextColor: "#fffcff",
+  darkTextColor: "#0e0f26",
+};
---
+++ src/styles/mixins.ts
@@ -0,0 +1,53 @@
+import { css } from "styled-components";
+import { colors } from "./colors";
+import { constants } from "./constants";
+
+const headingStyle = (dark = false) => css`
+  font-family: ${constants.defaultFontfamily};
+  color: ${dark ? colors.darkTextColor : colors.whiteTextColor};
+`;
+
+const overlay = () => css`
+  background: rgba(0, 0, 0, 0.6);
+`;
+
+const primaryBackgroundBox = (light = false) => css`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  background-color: ${colors.primaryColor};
+
+  background-image: ${light ? constants.bg1Light : constants.bg1Dark};
+
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const secondaryBackgroundBox = (light = false) => css`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  background-color: ${colors.primaryColor};
+
+  background-image: ${light ? constants.bg2Light : constants.bg2Dark};
+
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const contentBox = (darkTextColor = false) => css`
+  min-height: 100vh;
+  width: calc(100% - 58px);
+  transform: translateX(56px);
+  padding: 30px;
+  border: 5px solid red;
+  ${headingStyle(darkTextColor)}
+`;
+
+export const mixins = {
+  headingStyle,
+  overlay,
+  primaryBackgroundBox,
+  secondaryBackgroundBox,
+  contentBox,
+};
