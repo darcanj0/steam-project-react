0e92a94567096d63c374fdf92a1b72b559abba5d
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-08-28 00:11:09 -0300

    connection with online api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3001" });
+// const api = axios.create({ baseURL: "http://localhost:3001" });
+const api = axios.create({ baseURL: "https://steam-project-api.onrender.com" });
 
 export default api;
